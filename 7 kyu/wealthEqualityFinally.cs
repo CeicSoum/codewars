4518d86f3582bbc60903e785b14e9fe4e9c13272
CeicSoum <109542380+CeicSoum@users.noreply.github.com>
2022-07-24 22:14:05 -0400

    Update and rename Wealth equality, finally! to wealthEqualityFinally.cs
     Create Wealth equality, finally!
    
    The year is 2088 and the Radical Marxist Socialist People's Party (RMSPP) has just seized power in Brazil.
    
    Their first act in power is absolute wealth equality through coercive redistribution.
    
    Create a function that redistributes all wealth equally among all citizens.
    
    Wealth is represented as an array/list where every index is the wealth of a single citizen.
    The function should mutate the input such that every index has the same amount of wealth.
    
    See example:
    
        Input:
    
        [5, 10, 6]  >>> This represents:
                   # citizen 1 has wealth 5
                   # citizen 2 has wealth 10
                   # citizen 3 has wealth 6
    
        Should be after the test:
    
         [7, 7, 7] >>> wealth has now been equally redistributed
    
    Info:
    
        MUTATE the input array/list, don't return anything
        Input is guaranteed to hold at least 1 citizen
        Wealth of a citizen will be an integer with minimum equal to 0 (negative wealth is not possible)
        Handling of floating point error will not be tested

--- 7 kyu/wealthEqualityFinally.cs
+++ 7 kyu/wealthEqualityFinally.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Linq;
+
 public class Kata {
+
   public static void RedistributeWealth(double[] wealth) {
     double sum = wealth.Sum();
     double redistribution = sum / wealth.Length;
     Array.Clear(wealth, 0, wealth.Length);
     Array.Fill(wealth, redistribution);
+    
     if (wealth.Length > 1) {
-    Console.WriteLine(wealth.Length+" persons"+", sum total: "+sum+"$"+", "+redistribution+"$ per person.");
+    Console.WriteLine(wealth.Length + " persons" + ", sum total: " + sum + "$" + ", " + redistribution + "$ per person.");
     }
+    
     else {
-    Console.WriteLine(wealth.Length+" person"+", sum total: "+sum+"$");
+    Console.WriteLine(wealth.Length + " person" + ", sum total: " + sum + "$");
     }
+    
   }
 }
