b5e3d819697e0647c8628b0c2fd343822a8010c5
CeicSoum <109542380+CeicSoum@users.noreply.github.com>
2022-07-31 21:27:24 -0400

    Update alphabetPosition.cs

--- 6 kyu/alphabetPosition.cs
+++ 6 kyu/alphabetPosition.cs
@@ -8,14 +8,12 @@ public static string AlphabetPosition(string text) {
  Console.WriteLine(text);
   
  for (int i = 0; i < text.Length; i++) {
-    
   if (text[i].ToString().All(Char.IsLetter) == true) {
-   int num = (int)text[i] % 32;
-   newText += num.ToString();
-   newText += " ";
+    int num = (int)text[i] % 32;
+    newText += num.ToString();
+    newText += " ";
    }
-
-   }
+ }
   
    if (newText.Length > 0) {
     newText = newText.Remove(newText.Length - 1, 1);
