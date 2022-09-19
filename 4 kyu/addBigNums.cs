78c6764b3f7aebbe604c8fa86981665f6bcdc798
CeicSoum <109542380+CeicSoum@users.noreply.github.com>
2022-09-19 16:06:44 -0400

    Update addBigNums.cs
    https://www.codewars.com/kata/525f4206b73515bffb000b21/train/csharp

--- 4 kyu/addBigNums.cs
+++ 4 kyu/addBigNums.cs
@@ -1,13 +1,72 @@
 using System;
-using System.Linq;
-using System.Numerics;
 
 public class Kata {
-
+  
    public static string Add(string a, string b) {
-      BigInteger sum = BigInteger.Parse(a) + BigInteger.Parse(b);
-      Console.WriteLine(sum);
-      return sum.ToString();
+      int difference = 0;
+      int length = a.Length;
+      string result = "";
+      bool carry = false;
+
+      if (a.Length > b.Length) {
+         difference = a.Length - b.Length;
+         string num = "";
+
+         for (int i = 0; i < difference; i++) {
+            num += "0";
+         }
+
+         num += b;
+         b = num;
+      }
+
+      if (b.Length > a.Length) {
+         difference = b.Length - a.Length;
+         string num = "";
+
+         for (int j = 0; j < difference; j++) {
+            num += "0";
+         }
+
+         num += a;
+         a = num;
+      }
+
+      for (int k = a.Length - 1; k >= 0; k--) {
+         int count = 0;
+         count = ((a[k] - 48) + (b[k] - 48));
+
+         if (carry == true) {
+            count++;
+         }
+
+         if (count <= 9) {
+            carry = false;
+         }
+
+         if (count > 9) {
+            carry = true;
+            count -= 10;
+         }
+
+         result += count.ToString();
+
+         if (carry == true && k == 0) {
+            result += "1";
+         }
+
+      }
+
+      char[] arr = result.ToCharArray();
+      Array.Reverse(arr);
+
+      result = new String(arr);
+
+      if (result[0] == '0') {
+         result = result.Remove(0, 1);
+      }
+
+      Console.WriteLine(result);
+      return result;
    }
-   
 }
