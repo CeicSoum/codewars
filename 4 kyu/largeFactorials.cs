450bd06fe82fbc0d9cf96c65172d93270ba657c7
CeicSoum <109542380+CeicSoum@users.noreply.github.com>
2022-09-19 19:20:31 -0400

    Update largeFactorials.cs
    https://www.codewars.com/kata/557f6437bf8dcdd135000010/train/csharp

--- 4 kyu/largeFactorials.cs
+++ 4 kyu/largeFactorials.cs
@@ -1,17 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 public class Kata {
-  
-  public static string Factorial(int n) {
-    ulong m = Convert.ToUInt64(n);
-    ulong result = 1;
-    
-     while ( m != 1) {
-      result = result * m;
-      m -= 1;
-    }
-    
-    Console.WriteLine(result);
-    return result.ToString();
-  }
+
+   public static string Factorial(int n) {
+      List <int> res = new List <int>();
+      res.Add(1);
+      int car = 0;
+      string result = "";
+
+      for (int i = 2; i <= n; i++) {
+        
+         for (int j = 0; j < res.Count; j++) {
+            int temp = res[j] * i;
+            res[j] = (temp + car) % 10;
+            car = (temp + car) / 10;
+         }
+
+         while (car != 0) {
+            res.Add(car % 10);
+            car = car / 10;
+         }
+
+      }
+
+      for (int k = res.Count - 1; k >= 0; k--) {
+         result += res[k].ToString();
+      }
+     
+      Console.WriteLine(result);
+      return result;
+   }
 }
