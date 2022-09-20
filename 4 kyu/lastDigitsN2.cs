b699176550919f4aae0aec0bf316b21bc6062725
CeicSoum <109542380+CeicSoum@users.noreply.github.com>
2022-09-20 17:55:24 -0400

    Update lastDigitsN2.cs

--- 4 kyu/lastDigitsN2.cs
+++ 4 kyu/lastDigitsN2.cs
@@ -1,29 +1,39 @@
 using System;
-using System.Numerics;
 using System.Collections.Generic;
+using System.Numerics;
+using System.Linq;
 
 public class GreenNumbers {
-  
-   public static BigInteger Get(int n) {
-       List<BigInteger> nums = new List<BigInteger>();
-       BigInteger o;
-       nums.Add(0);
-       string green;
-       string compare;
-     
-        for (int i = 1; i <= 5000; i++) {
-          o = BigInteger.Pow(i, 2);
-          compare = o.ToString();
-          green = i.ToString();
-          int length = green.Length;
-          compare = compare.Substring(length-1);
+   
+  public static BigInteger Get(int n) {
+      
+     if (n == 1) {
+         return 1;
+      }
 
-           if (compare.Contains(green)) {
-            nums.Add(i); 
-          }     
+      SortedSet < BigInteger > list = new SortedSet < BigInteger > ();
+      int number = n + n / 5;
+      int digit = 2;
+      int count = 2;
+      BigInteger num = 5;
+      list.Add(num);
+      BigInteger num2 = 6;
+      list.Add(num2);
 
-        }
-       
-       return nums[n];
+      while (count < number) {
+         BigInteger numSquared = num * num;
+         BigInteger powOfTen = BigInteger.Pow(10, digit);
+         num = numSquared % powOfTen;
+         num2 = powOfTen + 1 - num;
+         list.Add(num);
+         list.Add(num2);
+         count += 2;
+         digit++;
+      }
+    
+      BigInteger[] bigIntegers = list.ToArray();
+      Console.WriteLine(bigIntegers[n - 2]);
+      return bigIntegers[n - 2];
    }
+
 }
