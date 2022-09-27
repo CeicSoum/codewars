2a9835a5838e0b0d0164659e000a0ef53b5ab66a
CeicSoum <109542380+CeicSoum@users.noreply.github.com>
2022-09-27 12:55:16 -0400

    Update binMulofThree.cs
    https://www.codewars.com/kata/54de279df565808f8b00126a/train/csharp

--- 4 kyu/binMulofThree.cs
+++ 4 kyu/binMulofThree.cs
@@ -4,7 +4,7 @@ using System.Text.RegularExpressions;
 public class BinaryRegex {
 
   public static Regex MultipleOf3() {
-    return new Regex("^(0|(0|1(01*0)*1)+)$");
+    return new Regex("^(0|1(01*0)*1)+$");
   }
   
 }
